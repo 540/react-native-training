891e6064e0f5f11ed224fccdecf75e2b64ed54b5
Gorka <gorka@540deg.com>
2023-02-08 17:41:44 +0100

    Use react-query

--- App.tsx
+++ App.tsx
@@ -1,66 +1,13 @@
-import { Intro } from './src/Intro'
-import { Header } from './src/Header'
-import { List } from './src/List'
-import { Footer } from './src/Footer'
-import { Layout } from './src/Layout'
-import { useEffect, useState } from 'react'
-import { api, Character, Comic } from './api'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { Root } from './src/Root'
 
-const App = () => {
-  const [comics, setComics] = useState<Comic[]>([])
-  const [characters, setCharacters] = useState<Character[]>([])
-  const [firstCharacterFilter, setFirstCharacterFilter] = useState<string | undefined>(undefined)
-  const [secondCharacterFilter, setSecondCharacterFilter] = useState<string | undefined>(undefined)
+const queryClient = new QueryClient()
 
-  useEffect(() => {
-    const fetchCharacters = async () => {
-      setCharacters(await api.characters())
-    }
-
-    fetchCharacters()
-  }, [])
-
-  useEffect(() => {
-    const fetchComics = async () => {
-      if (firstCharacterFilter === undefined || secondCharacterFilter === undefined) {
-        return
-      }
-
-      const [firstCharacterComics, secondCharacterComics] = await Promise.all([
-        api.comics(firstCharacterFilter),
-        api.comics(secondCharacterFilter)
-      ])
-
-      const commonComics = firstCharacterComics.filter(comic1 =>
-        secondCharacterComics.some(comic2 => comic1.id === comic2.id)
-      )
-
-      setComics(commonComics)
-    }
-
-    fetchComics()
-  }, [firstCharacterFilter, secondCharacterFilter])
-
-  return (
-    <Layout>
-      <Intro />
-      <Header
-        characters={characters}
-        firstCharacterFilter={firstCharacterFilter}
-        secondCharacterFilter={secondCharacterFilter}
-        onChangeFirstCharacter={setFirstCharacterFilter}
-        onChangeSecondCharacter={setSecondCharacterFilter}
-        onClear={() => {
-          setComics([])
-          setFirstCharacterFilter(undefined)
-          setSecondCharacterFilter(undefined)
-        }}
-      />
-      <List comics={comics} />
-      <Footer comicCount={comics.length} />
-    </Layout>
-  )
-}
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <Root />
+  </QueryClientProvider>
+)
 
 // eslint-disable-next-line import/no-default-export
 export default App
---
+++ src/Root.tsx
@@ -0,0 +1,68 @@
+import { Intro } from './Intro'
+import { Header } from './Header'
+import { List } from './List'
+import { Footer } from './Footer'
+import { Layout } from './Layout'
+import { useState } from 'react'
+import { api } from '../api'
+import { useQuery } from 'react-query'
+import { ActivityIndicator, StyleSheet } from 'react-native'
+
+const fetchComics = async (firstCharacterFilter: string, secondCharacterFilter: string) => {
+  if (firstCharacterFilter === undefined || secondCharacterFilter === undefined) {
+    return []
+  }
+
+  const [firstCharacterComics, secondCharacterComics] = await Promise.all([
+    api.comics(firstCharacterFilter, 3),
+    api.comics(secondCharacterFilter, 3)
+  ])
+
+  const commonComics = firstCharacterComics.filter(comic1 =>
+    secondCharacterComics.some(comic2 => comic1.id === comic2.id)
+  )
+
+  return commonComics
+}
+
+export const Root = () => {
+  const [firstCharacterFilter, setFirstCharacterFilter] = useState<string | undefined>(undefined)
+  const [secondCharacterFilter, setSecondCharacterFilter] = useState<string | undefined>(undefined)
+
+  const { data: characters } = useQuery('characters', api.characters, { initialData: [] })
+  const {
+    data: comics,
+    isFetching: isFetchingComics,
+    remove: clearComics
+  } = useQuery(
+    ['comics', firstCharacterFilter, secondCharacterFilter],
+    () => fetchComics(firstCharacterFilter, secondCharacterFilter),
+    { initialData: [] }
+  )
+
+  return (
+    <Layout>
+      <Intro />
+      <Header
+        characters={characters}
+        firstCharacterFilter={firstCharacterFilter}
+        secondCharacterFilter={secondCharacterFilter}
+        onChangeFirstCharacter={setFirstCharacterFilter}
+        onChangeSecondCharacter={setSecondCharacterFilter}
+        onClear={() => {
+          clearComics()
+          setFirstCharacterFilter(undefined)
+          setSecondCharacterFilter(undefined)
+        }}
+      />
+      {isFetchingComics ? <ActivityIndicator style={styles.loader} size="large" /> : <List comics={comics} />}
+      <Footer comicCount={comics.length} />
+    </Layout>
+  )
+}
+
+const styles = StyleSheet.create({
+  loader: {
+    flex: 1
+  }
+})
