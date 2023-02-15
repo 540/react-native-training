6b3527d1794ddd78b1c270c19456b8610f0491e3
Sabina <sabina.hurtado@540deg.com>
2023-02-15 16:33:54 +0100

    see logs and api calls in flipper

--- src/ui/Home/_components/Header.tsx
+++ src/ui/Home/_components/Header.tsx
@@ -2,6 +2,7 @@ import { Button, Platform, StyleSheet, Text, View } from 'react-native'
 import { Picker } from '@react-native-picker/picker'
 import { useTranslation } from 'react-i18next'
 import { Character } from 'core/character/domain/character'
+import { useQuery } from 'react-query'
 
 interface Props {
   characters: Character[]
@@ -21,6 +22,10 @@ export const Header = ({
   onClear
 }: Props) => {
   const { t } = useTranslation()
+
+  const { refetch } = useQuery('dogs', () => fetch('https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all'))
+  //eslint-disable-next-line
+  console.log('**** I am rendering Header')
 
   return (
     <View style={styles.controlsContainer}>
@@ -43,7 +48,13 @@ export const Header = ({
           <Picker.Item key={character.id} label={character.name} value={character.id} />
         ))}
       </Picker>
-      <Button title={t('home.button.reset')} onPress={onClear} />
+      <Button
+        title={t('home.button.reset')}
+        onPress={() => {
+          onClear()
+          refetch()
+        }}
+      />
     </View>
   )
 }
